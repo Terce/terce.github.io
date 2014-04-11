f95e1de65d24b65082b214d768049cfb8aa21c44
Terce <salvador.ahedo@gmail.com>
2014-04-11 13:11:00 -0500

    a

---
+++ jquery/jquery.java
@@ -0,0 +1,381 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Online Prescriptions</title>
+   <link type="text/css" rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1/themes/smoothness/jquery-ui.css"/>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1/jquery-ui.min.js"></script>
+<script>
+
+  $(function() {
+    $( "#menu" ).menu();
+  });
+  </script>
+  
+
+</head>
+    <style type="text/css">
+    .head{
+		background-color:#0080FF;
+		position:fixed;
+		width:100%;
+		height:100px;
+		padding:20px;
+		color:#FFFFFF;
+		z-index:1;
+		opacity: .90;		
+	}
+	
+	.p{
+		position:absolute;
+		top:100px;
+		left:30px;
+	}
+	.titlebox{
+		color:#0080FF;
+		text-decoration:underline;
+	}
+
+	</style>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+	<div class="head">
+	    <h1>Online Prescriptions</h1>
+	</div>
+	<div id="container">
+		<div class="p">
+			<div class="row">
+				<div class="col-lg-8">
+					<h1 class="titlebox">Diagnose:</h1>
+					<p>
+						Lucy is a 34-year old single mother who has recently been diagnosed 
+						with a cranial tumour in the right frontal lobe. The diagnosis explains 
+						her symptoms of persistent and worsening headache over the last four weeks, 
+						which have led her to resign from work and rely more on her mother for support 
+						and care. 
+					</p>	
+					<p>
+						Lucy has also experienced symptoms of increased intracranial pressure, 
+						such as nausea, vomiting, and mild photophobia. Hence, it is likely that the tumour 
+						is a space-occupying lesion, which is exerting the oedema effect and causing the 
+						symptoms that Lucy is experiencing.
+					</p>
+					<p>
+						Taking her age and sex into consideration, 
+						the lesion is most likely to be a primary lesion, single and benign in nature. 
+						In addition, given that Lucy's father died 15 years ago of stroke related causes, 
+						her mother and her sister both have cardiovascular illness, and Lucy has HERNS syndrome, 
+						there is a high probability that the tumour has a vascular cause.
+					</p>
+					<h1 class="titlebox">Treatment:</h1>
+					<p>Date: March 9, 2009</p>
+
+					<p>Name: Emily Watson Age: 27</p>
+					<p>Address : 2459 N. Harbor Ave. San Diego, CA 92126 D.O.B: January 24, 1981</p> 
+
+					<p>Rx: Doxycycline 100 mg</p>
+					<p>Disp: #14</p>
+					<p>Sig: Take 1 capsule bid x 7 days</p>
+
+					<p>Refills: 0 Substitution Permissible</p>
+					<p>( note: if premade RX form denotes 0,1,2,3 you can cross out all but desired # to make difficult to alter.)</p>
+					<p>Dr. Jane Smith</p>
+					<p>DEA #: AN12543</p>
+				</div>
+				<div class="col-lg-1">
+				</div>
+				<div class="col-lg-2">
+					<h3><iframe src="http://free.timeanddate.com/clock/i41pp75r/n92/fs20/ftb/tt1/tw0" frameborder="0" width="189" height="25"></iframe></h3>
+					<p>
+						<img src="http://c.dryicons.com/images/icon_sets/shine_icon_set/png/256x256/business_user.png" alt="Smiley face" height="130" width="130">
+					</p>
+					<p>
+						Salvador Ahedo Leyva
+					</p>
+					<p>
+						Dr. House
+					</p>
+					<p>
+						<h3>Medical History</h3>
+					</p>
+					<p>
+						<ul id="menu">
+  							<li>
+    						<a href="#">January</a>
+    						<ul>
+      						<li>
+        						<a href="#">1/3</a>
+        						<ul>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+        						</ul>
+      						</li>
+      						<li>
+        						<a href="#">2/3</a>
+        						<ul>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+        						</ul>
+      						</li>
+      						<li><a href="#">3/3</a></li>
+    						</ul>
+  							</li>
+  							<li>
+    						<a href="#">February</a>
+    						<ul>
+      						<li>
+        						<a href="#">1/3</a>
+        						<ul>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+        						</ul>
+      						</li>
+      						<li>
+        						<a href="#">2/3</a>
+        						<ul>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+        						</ul>
+      						</li>
+      						<li><a href="#">3/3</a></li>
+    						</ul>
+  							</li>
+  							<li>
+    						<a href="#">March</a>
+    						<ul>
+      						<li>
+        						<a href="#">1/3</a>
+        						<ul>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+        						</ul>
+      						</li>
+      						<li>
+        						<a href="#">2/3</a>
+        						<ul>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+        						</ul>
+      						</li>
+      						<li><a href="#">3/3</a></li>
+    						</ul>
+  							</li>
+  							<li>
+    						<a href="#">April</a>
+    						<ul>
+      						<li>
+        						<a href="#">1/3</a>
+        						<ul>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+        						</ul>
+      						</li>
+      						<li>
+        						<a href="#">2/3</a>
+        						<ul>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+        						</ul>
+      						</li>
+      						<li><a href="#">3/3</a></li>
+    						</ul>
+  							</li>
+  							<li>
+    						<a href="#">May</a>
+    						<ul>
+      						<li>
+        						<a href="#">1/3</a>
+        						<ul>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+        						</ul>
+      						</li>
+      						<li>
+        						<a href="#">2/3</a>
+        						<ul>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+        						</ul>
+      						</li>
+      						<li><a href="#">3/3</a></li>
+    						</ul>
+  							</li>
+  							<li>
+    						<a href="#">June</a>
+    						<ul>
+      						<li>
+        						<a href="#">1/3</a>
+        						<ul>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+        						</ul>
+      						</li>
+      						<li>
+        						<a href="#">2/3</a>
+        						<ul>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+        						</ul>
+      						</li>
+      						<li><a href="#">3/3</a></li>
+    						</ul>
+  							</li>
+  							<li>
+    						<a href="#">July</a>
+    						<ul>
+      						<li>
+        						<a href="#">1/3</a>
+        						<ul>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+        						</ul>
+      						</li>
+      						<li>
+        						<a href="#">2/3</a>
+        						<ul>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+        						</ul>
+      						</li>
+      						<li><a href="#">3/3</a></li>
+    						</ul>
+  							</li>
+  							<li>
+    						<a href="#">August</a>
+    						<ul>
+      						<li>
+        						<a href="#">1/3</a>
+        						<ul>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+        						</ul>
+      						</li>
+      						<li>
+        						<a href="#">2/3</a>
+        						<ul>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+        						</ul>
+      						</li>
+      						<li><a href="#">3/3</a></li>
+    						</ul>
+  							</li>
+  							<li>
+    						<a href="#">September</a>
+    						<ul>
+      						<li>
+        						<a href="#">1/3</a>
+        						<ul>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+        						</ul>
+      						</li>
+      						<li>
+        						<a href="#">2/3</a>
+        						<ul>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+        						</ul>
+      						</li>
+      						<li><a href="#">3/3</a></li>
+    						</ul>
+  							</li>
+  							<li>
+  							<a href="#">October</a>
+    						<ul>
+      						<li>
+        						<a href="#">1/3</a>
+        						<ul>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+        						</ul>
+      						</li>
+      						<li>
+        						<a href="#">2/3</a>
+        						<ul>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+        						</ul>
+      						</li>
+      						<li><a href="#">3/3</a></li>
+    						</ul>
+  							</li>
+  							<li>
+  							<a href="#">November</a>
+    						<ul>
+      						<li>
+        						<a href="#">1/3</a>
+        						<ul>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+        						</ul>
+      						</li>
+      						<li>
+        						<a href="#">2/3</a>
+        						<ul>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+        						</ul>
+      						</li>
+      						<li><a href="#">3/3</a></li>
+    						</ul>
+  							</li>
+  							<li>
+  							<a href="#">December</a>
+    						<ul>
+      						<li>
+        						<a href="#">1/3</a>
+        						<ul>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+        						</ul>
+      						</li>
+      						<li>
+        						<a href="#">2/3</a>
+        						<ul>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+          							<li><a href="#">.</a></li>
+        						</ul>
+      						</li>
+      						<li><a href="#">3/3</a></li>
+    						</ul>
+  							</li>
+							</ul>
+					</p>
+				</div>
+				<div class="col-lg-2">
+				</div>
+			</div>
+		</div>
+		
+	</div>
+  </body>
+</html>
