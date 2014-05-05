646568314f823c88d89c4a144090a39575119917
Terce <salvador.ahedo@gmail.com>
2014-05-05 09:28:19 -0500

    q3

---
+++ final/maincontroller.js
@@ -0,0 +1,25 @@
+app.controller("MainController", function($scope){
+		$scope.people = [
+			{
+				id: 0,
+				name: 'Lycy Watson',
+				live: true
+			},
+			{
+				id: 1,
+				name: 'Chris Redfield',
+				live: true
+			},
+			{
+				id: 2,
+				name: 'Harry Potter',
+				live: true
+			},
+			{
+				id: 3,
+				name: 'Allyce Beasley',
+				live: true
+			}
+		];
+		
+});
